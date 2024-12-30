1ed8798ecdb4160974fc7e0bd75ff5dfa42d4f79
bodik1987 <bsshul@gmail.com>
2024-12-30 20:46:32 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <main className="MAX-W-MD mx-auto p-3">
+      <div className="font-medium text-2xl">
+        <h3 className="mb-2 border-b border-[#B2B2B2] pb-1 pl-1">PRIORITIES</h3>
+
+        <p className="font-myFont border-b border-[#DADCDB] pb-1 pl-1">
+          1. <span className="text-[#2730AB]">history assignment</span>
+        </p>
+        <p className="font-myFont border-b border-[#DADCDB] pb-1 pl-1">
+          2. <span className="text-[#2730AB]">Return books</span>
+        </p>
+        <p className="font-myFont border-b border-[#DADCDB] pb-1 pl-1">
+          3. <span className="text-[#2730AB]"></span>
+        </p>
+      </div>
+    </main>
+  );
+}
