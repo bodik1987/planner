e5169d6e8d8f756588d8275bd23b76468bfc7ae5
bodik1987 <bsshul@gmail.com>
2024-12-30 23:36:11 +0100

    Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 export default function App() {
   return (
     <main className="MAX-W-MD mx-auto p-3">
-      <div className="font-medium text-2xl">
-        <h3 className="mb-2 border-b border-[#B2B2B2] pb-1 pl-1">PRIORITIES</h3>
+      <div className="">
+        <h3 className="font-medium font-mazzardH tracking-widest mb-2 border-b border-[#B2B2B2] pb-1 pl-1">
+          PRIORITIES
+        </h3>
 
-        <p className="font-myFont border-b border-[#DADCDB] pb-1 pl-1">
-          1. <span className="text-[#2730AB]">history assignment</span>
+        <p className="text-3xl font-myFont border-b border-[#DADCDB] pb-1 pl-1">
+          1. <input className="text-[#2730AB] outline-none" type="text" />
         </p>
-        <p className="font-myFont border-b border-[#DADCDB] pb-1 pl-1">
+        <p className="text-3xl font-myFont border-b border-[#DADCDB] pb-1 pl-1">
           2. <span className="text-[#2730AB]">Return books</span>
         </p>
-        <p className="font-myFont border-b border-[#DADCDB] pb-1 pl-1">
+        <p className="text-3xl font-myFont border-b border-[#DADCDB] pb-1 pl-1">
           3. <span className="text-[#2730AB]"></span>
         </p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
     extend: {
       fontFamily: {
         myFont: ["myFont", "sans-serif"],
+        mazzardH: ["mazzardH", "sans-serif"],
       },
     },
   },
