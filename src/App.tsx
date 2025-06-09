391fafd368b2a196bfa2a51a554a03a4ca853d68
bodik1987 <bsshul@gmail.com>
2025-06-09 21:43:12 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+type DayItem = {
+  id: number;
+  name: string;
+  content?: string;
+};
+
+export default function App() {
+  const [expandedDay, setExpandedDay] = useState<number | null>(null);
+
+  const daysOfWeek: DayItem[] = [
+    { id: 1, name: "ПОНЕДЕЛЬНИК", content: "Контент понедельника..." },
+    { id: 2, name: "ВТОРНИК", content: "Контент вторника..." },
+    { id: 3, name: "СРЕДА", content: "Контент среды..." },
+    { id: 4, name: "ЧЕТВЕРГ", content: "Контент четверга..." },
+    { id: 5, name: "ПЯТНИЦА", content: "Контент пятницы..." },
+    { id: 6, name: "СУББОТА", content: "Контент субботы..." },
+    { id: 7, name: "ВОСКРЕСЕНЬЕ", content: "Контент воскресенья..." },
+  ];
+
+  const toggleDay = (id: number) => {
+    setExpandedDay(expandedDay === id ? null : id);
+  };
+
+  const getDayHeight = (id: number): string => {
+    if (expandedDay === null) {
+      return "h-[calc(100%/7)]";
+    }
+    if (id === expandedDay) {
+      return "h-1/2";
+    }
+    return `h-[calc(50%/${daysOfWeek.length - 1})]`;
+  };
+
+  return (
+    <main className="h-screen flex flex-col">
+      {daysOfWeek.map((day) => (
+        <div
+          key={day.id}
+          className={`${getDayHeight(
+            day.id
+          )} border-y border-[#B4B4B4] cursor-pointer`}
+          onClick={() => toggleDay(day.id)}
+        >
+          <div
+            className={`${
+              !expandedDay && "justify-center"
+            } max-w-3xl w-full mx-auto h-full py-4 px-8 flex flex-col `}
+          >
+            <div className="">
+              <div className="font-bold font-jet-medium text-[#3A3A3A] text-3xl">
+                {day.name}
+              </div>
+              {expandedDay === day.id && (
+                <div className="mt-2 text-gray-600">{day.content}</div>
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </main>
+  );
+}
